924eee90eb338dd6119a5ca564043135ba8a581a
RicardoValadas13 <benjamim4work@gmail.com>
2023-09-13 15:59:14 +0100

    storepath changes

--- srcs/pipex_utils.c
+++ srcs/pipex_utils.c
@@ -3,8 +3,11 @@
 //ft_check_args 
 #include <unistd.h>
 #include <fcntl.h>
-char*  ft_store_path(char **envp, char *cmd)
+char*  ft_store_path(char **envp)
 {
+  while (envp != NULL && ft_strncmp("PATH", *envp, 4))
+    envp++;
+  retunr(*envp + 5);
 }
 void  ft_check_args(char **args, char **envp)
 {
